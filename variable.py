84faac1eef84bd1a34954ebf952e9620b43e7df2
md. Alamin Faraji <alaminice1617@gmail.com>
2023-09-23 22:58:55 +0600

    basic data type int python

---
+++ variable.py
@@ -0,0 +1,15 @@
+age=45
+interest_rate=2.5
+name="alamin"
+district = 'b baria'
+is_single=True
+is_sleeping = False
+
+print(age)
+print(type(interest_rate))
+# print(type(is_single))
+"""alt+shift+a:doc string or multi line comment  """
+print("kodom ali" + " " + " kacha badam")
+
+text=f" kodom ali {age} linving in {district}"
+print(text)
