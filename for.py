d93e56fc567db0d8998d8b6de5747a01e0906d20
md. Alamin Faraji <alaminice1617@gmail.com>
2023-09-24 07:16:07 +0600

    for loop

---
+++ for.py
@@ -0,0 +1,20 @@
+# nums
+# for(int i=0; i<5; i++){
+# int num=nums[i]
+# }
+""" 
+numbers=[45, 3453, 546,56,6,7]
+sum=0
+for num in numbers:
+    print(num)
+    sum=sum+num
+    if sum>20:
+        print('bigger sum', sum)
+print(sum)
+ """
+text="pagla howar"
+for char in text:
+    print(char)
+
+for i in range(1, 10, 2):
+    print(i)
---
+++ summary.py
@@ -0,0 +1,19 @@
+num1=int(input("first number : "))
+num2=int(input("second number : "))
+num3=int(input("third number : "))
+
+if num1>num2 and num1>num2:
+    print("largest number: ", num1)
+elif num2>num1 and num2>num3:
+    print("largest number: ", num2)
+else:
+    print("largest number: ", num3)
+num=39
+while num<=68:
+    if num%2==1:
+        print(num)
+    num+=1
+
+for num in range(39, 68, 2):
+    print(num)
+
